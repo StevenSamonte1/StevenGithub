54a2974761311441a8a876522a8171c034988bbc
Samonte <s.samonte3252@student.leedsbeckett.ac.uk>
2023-10-01 18:58:10 +0100

    Exercise01

---
+++ SamontePractical1.py
@@ -0,0 +1,23 @@
+print("the program has executed")
+print("Hello, what are you doing?")
+print("My name is steven")
+print("hi")
+
+print("ten divided by zero is", 10/0)
+
+45 + 20
+10 + 20 - 15
+10 * 5
+100 / 33
+100 // 33
+10 ** 2
+10 % 3
+10 + 5 * 2
+10 - 5 * 10 + 5
+5 * 10 ** 2
+(10 + 5) * 2
+10 - 5 * (10 + 5)
+(5 * 10) ** 2
+12 + (5 * 2 + 3)
+12 + (5 * (2 + 3))
+
